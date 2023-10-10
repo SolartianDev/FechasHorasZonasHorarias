cb537a7b08ca7ba8d26941125ef1641ff6caf518
SolartianDev <david.solartemoreno@gmail.com>
2023-10-10 16:27:16 -0500

    modulo 12.

--- src/com/fechasHorasZonasHorarias/practica/SalidasLleadas.java
+++ src/com/fechasHorasZonasHorarias/practica/SalidasLleadas.java
@@ -1,3 +1,4 @@
+// Paso 1: Importar las clases y métodos necesarios
 package com.fechasHorasZonasHorarias.practica;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -6,61 +7,55 @@ import static java.time.Month.*;
 
 public class SalidasLleadas {
     public static void main(String[] args) {
-    // Utilizando la siguiente información:
-    //   * Usa America/New_York como zona horaria para el Aeropuerto Logan de Boston (BOS).
-    //   * Usa America/Los_Angeles como zona horaria para el Aeropuerto de San Francisco (SFO).
-    //   * Usa Asia/Calcuta como zona horaria para el Aeropuerto Internacional de Bengaluru en Bangalore (BLR).
-    ZoneId SFO = ZoneId.of("America/Los_Angeles");
-    ZoneId BOS = ZoneId.of("America/New_York");
-    ZoneId BLR = ZoneId.of("Asia/Calcutta");
+        // Paso 2: Declarar la clase y el método principal
 
-    // Vuelo 123, San Francisco a Boston, sale de SFO a las 10:30 PM el 13 de junio de 2014
-    // El vuelo dura 5 horas 30 minutos
-    //   ¿Cuál es la hora local en Boston cuando despega el vuelo?
-    //   ¿Cuál es la hora local en el Aeropuerto Logan de Boston cuando llega el vuelo?
-    //   ¿Cuál es la hora local en San Francisco cuando llega el vuelo?
-    LocalDateTime salida = LocalDateTime.of(2014, JUNE, 13, 22, 30);
-    ZonedDateTime salidaSFO = ZonedDateTime.of(salida, SFO);
-    System.out.println("El vuelo 123 sale de SFO a las:   " + salidaSFO);
-    ZonedDateTime horaSalidaEnBOS = salidaSFO.toOffsetDateTime().atZoneSameInstant(BOS);
-    System.out.println("Hora local en BOS al despegue: " + horaSalidaEnBOS);
-    System.out.println("Tiempo de vuelo: 5 horas 30 minutos");
-    ZonedDateTime llegadaBOS = salidaSFO.plusHours(5).plusMinutes(30).toOffsetDateTime().atZoneSameInstant(BOS);
-    System.out.println("El vuelo 123 llega a BOS:      " + llegadaBOS);
-    ZonedDateTime horaLlegadaEnSFO = llegadaBOS.toOffsetDateTime().atZoneSameInstant(SFO);
-    System.out.println("Hora local en SFO a la llegada:   " + horaLlegadaEnSFO);
-    System.out.println("");
+        // Paso 3: Definir zonas horarias para los aeropuertos
+        // Utilizando la información proporcionada.
 
-    // Vuelo 456, San Francisco a Bangalore, India, sale de SFO el sábado a las 10:30 PM el 28 de junio de 2014
-    // El tiempo de vuelo es de 22 horas
-    //   ¿Llegará el viajero a una reunión en Bangalore el lunes a las 9 AM hora local?
-    //   ¿Podrá el viajero llamar a su esposo a una hora razonable?
-    salida = LocalDateTime.of(2014, JUNE, 28, 22, 30);
-    salidaSFO = ZonedDateTime.of(salida, SFO);
-    System.out.println("El vuelo 456 sale de SFO a las:    " + salidaSFO);
-    ZonedDateTime horaSalidaEnBLR = salidaSFO.toOffsetDateTime().atZoneSameInstant(BLR);
-    System.out.println("Hora local en BLR al despegue: " + horaSalidaEnBLR);
-    System.out.println("Tiempo de vuelo: 22 horas");
-    ZonedDateTime llegadaBLR = salidaSFO.plusHours(22).toOffsetDateTime().atZoneSameInstant(BLR);
-    System.out.println("El vuelo 456 llega a BLR:      " + llegadaBLR);
-    horaLlegadaEnSFO = llegadaBLR.toOffsetDateTime().atZoneSameInstant(SFO);
-    System.out.println("Hora local en SFO a la llegada:   " + horaLlegadaEnSFO);
-    System.out.println("");
+        ZoneId SFO = ZoneId.of("America/Los_Angeles");
+        ZoneId BOS = ZoneId.of("America/New_York");
+        ZoneId BLR = ZoneId.of("Asia/Calcutta");
 
-    // Vuelo 123, San Francisco a Boston, sale de SFO a las 10:30 PM el sábado, 1 de noviembre de 2014
-    // El tiempo de vuelo es de 5 horas 30 minutos.
-    //   ¿En qué día y hora llega el vuelo a Boston?
-    //   ¿Qué sucedió?
-    salida = LocalDateTime.of(2014, NOVEMBER, 1, 22, 30);
-    salidaSFO = ZonedDateTime.of(salida, SFO);
-    System.out.println("El vuelo 123 sale de SFO a las:    " + salidaSFO);
-    horaSalidaEnBOS = salidaSFO.toOffsetDateTime().atZoneSameInstant(BOS);
-    System.out.println("Hora local en BOS al despegue: " + horaSalidaEnBOS);
-    System.out.println("Tiempo de vuelo: 5 horas 30 minutos");
-    llegadaBOS = salidaSFO.plusHours(5).plusMinutes(30).toOffsetDateTime().atZoneSameInstant(BOS);
-    System.out.println("El vuelo 123 llega a BOS:      " + llegadaBOS);
-    horaLlegadaEnSFO = llegadaBOS.toOffsetDateTime().atZoneSameInstant(SFO);
-    System.out.println("Hora local en SFO a la llegada:   " + horaLlegadaEnSFO);
-    System.out.println("");
+        // Paso 4: Definir la información del vuelo 123, San Francisco a Boston.
+
+        LocalDateTime salida = LocalDateTime.of(2014, JUNE, 13, 22, 30);
+        ZonedDateTime salidaSFO = ZonedDateTime.of(salida, SFO);
+        System.out.println("El vuelo 123 sale de SFO a las:   " + salidaSFO);
+        ZonedDateTime horaSalidaEnBOS = salidaSFO.toOffsetDateTime().atZoneSameInstant(BOS);
+        System.out.println("Hora local en BOS al despegue: " + horaSalidaEnBOS);
+        System.out.println("Tiempo de vuelo: 5 horas 30 minutos");
+        ZonedDateTime llegadaBOS = salidaSFO.plusHours(5).plusMinutes(30).toOffsetDateTime().atZoneSameInstant(BOS);
+        System.out.println("El vuelo 123 llega a BOS:      " + llegadaBOS);
+        ZonedDateTime horaLlegadaEnSFO = llegadaBOS.toOffsetDateTime().atZoneSameInstant(SFO);
+        System.out.println("Hora local en SFO a la llegada:   " + horaLlegadaEnSFO);
+        System.out.println("");
+
+        // Paso 5: Definir la información del vuelo 456, San Francisco a Bangalore, India.
+
+        salida = LocalDateTime.of(2014, JUNE, 28, 22, 30);
+        salidaSFO = ZonedDateTime.of(salida, SFO);
+        System.out.println("El vuelo 456 sale de SFO a las:    " + salidaSFO);
+        ZonedDateTime horaSalidaEnBLR = salidaSFO.toOffsetDateTime().atZoneSameInstant(BLR);
+        System.out.println("Hora local en BLR al despegue: " + horaSalidaEnBLR);
+        System.out.println("Tiempo de vuelo: 22 horas");
+        ZonedDateTime llegadaBLR = salidaSFO.plusHours(22).toOffsetDateTime().atZoneSameInstant(BLR);
+        System.out.println("El vuelo 456 llega a BLR:      " + llegadaBLR);
+        horaLlegadaEnSFO = llegadaBLR.toOffsetDateTime().atZoneSameInstant(SFO);
+        System.out.println("Hora local en SFO a la llegada:   " + horaLlegadaEnSFO);
+        System.out.println("");
+
+        // Paso 6: Definir la información del vuelo 123, San Francisco a Boston, en noviembre.
+
+        salida = LocalDateTime.of(2014, NOVEMBER, 1, 22, 30);
+        salidaSFO = ZonedDateTime.of(salida, SFO);
+        System.out.println("El vuelo 123 sale de SFO a las:    " + salidaSFO);
+        horaSalidaEnBOS = salidaSFO.toOffsetDateTime().atZoneSameInstant(BOS);
+        System.out.println("Hora local en BOS al despegue: " + horaSalidaEnBOS);
+        System.out.println("Tiempo de vuelo: 5 horas 30 minutos");
+        llegadaBOS = salidaSFO.plusHours(5).plusMinutes(30).toOffsetDateTime().atZoneSameInstant(BOS);
+        System.out.println("El vuelo 123 llega a BOS:      " + llegadaBOS);
+        horaLlegadaEnSFO = llegadaBOS.toOffsetDateTime().atZoneSameInstant(SFO);
+        System.out.println("Hora local en SFO a la llegada:   " + horaLlegadaEnSFO);
+        System.out.println("");
     }
 }
--- src/com/fechasHorasZonasHorarias/practica/TiempoEntreFechas.java
+++ src/com/fechasHorasZonasHorarias/practica/TiempoEntreFechas.java
@@ -1,6 +1,6 @@
+// Paso 1: Importar las clases y métodos necesarios
 package com.fechasHorasZonasHorarias.practica;
-
- import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
@@ -11,38 +11,58 @@ import java.time.format.DateTimeParseException;
 public class TiempoEntreFechas {
 
   public static void main(String[] args) {
+    // Paso 2: Declarar la clase y el método principal
+
+    // Paso 3: Definir el formato de fecha y crear una variable para almacenar una fecha.
     String formatoFecha = "MMMM d, yyyy";
     LocalDate unaFecha = null;
 
+    // Paso 4: Inicializar una variable booleana para verificar la validez de la cadena de fecha.
     boolean cadenaValida = false;
+
+    // Paso 5: Crear un formateador de fecha y un lector de entrada (BufferedReader).
     DateTimeFormatter formateador = DateTimeFormatter.ofPattern(formatoFecha);
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // Paso 6: Usar un bucle para solicitar una fecha hasta que se ingrese una cadena válida.
     while (!cadenaValida) {
       System.out.print("Ingrese una fecha (" + formatoFecha + "): ");
       try {
+        // Paso 7: Leer la fecha ingresada como cadena y tratar de convertirla a un objeto LocalDate.
         String fechaIngresada = br.readLine();
         unaFecha = LocalDate.parse(fechaIngresada, formateador);
         cadenaValida = true;
       } catch (IOException | DateTimeParseException ex) {
+        // Paso 8: Capturar excepciones de entrada incorrecta y mantener cadenaValida como falso.
         cadenaValida = false;
       }
     }
 
+    // Paso 9: Imprimir la fecha ingresada.
     System.out.println("Fecha ingresada: " + unaFecha.format(formateador));
+
+    // Paso 10: Obtener la fecha actual.
     LocalDate ahora = LocalDate.now();
     Period entreFechas;
+
+    // Paso 11: Calcular el período de tiempo (años, meses y días) entre las dos fechas.
     if (unaFecha.isBefore(ahora)) {
       entreFechas = Period.between(unaFecha, ahora);
     } else {
       entreFechas = Period.between(ahora, unaFecha);
     }
+
+    // Paso 12: Extraer los componentes del período (años, meses y días).
     int años = entreFechas.getYears();
     int meses = entreFechas.getMonths();
     int días = entreFechas.getDays();
+
+    // Paso 13: Imprimir la diferencia de tiempo entre las dos fechas.
     System.out.println("Hay " + años + " años, " 
             + meses + " meses, " 
             + días + " días entre ahora y la fecha ingresada.");
   }
 }
+
 
 
